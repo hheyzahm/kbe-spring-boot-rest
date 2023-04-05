3acdf7fc35e849b6d27c6a7f1dba53505d3bf86b
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-04-05 09:50:51 +0500

    Initial commit

---
+++ src/main/java/springboot/kberest/KbeSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package springboot.kberest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeSpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbeSpringBootRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/kberest/KbeSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.kberest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeSpringBootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
